f13f9cd9b924a0c79a4bc140faf0eea51b57cd57
Linux Fight <kiko13024@gmail.com>
2023-01-27 22:14:35 +0300

    added shorts support

--- utils/get_video_id.py
+++ utils/get_video_id.py
@@ -3,7 +3,7 @@ import re
 
 def get_video_id(url: str):
     regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.'
-                       r'(com|be)/(watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
+                       r'(com|be)/(shorts/|watch\?v=|embed/|v/|.+\?v=)?(?P<id>[A-Za-z0-9\-=_]{11})')
 
     match = regex.match(url)
 
